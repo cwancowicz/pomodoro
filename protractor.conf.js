3e39990e5e4ef789173dd14b86e19ff65dcc4c58
Chris Wancowicz <cwancowicz@gmail.com>
2018-05-06 10:48:44 -0400

    got tests to pass

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,6 +2,7 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['test/pomodoro-test.js']
+    // ,
     // capabilities: {
     //     browserName: 'firefox'
     // }
